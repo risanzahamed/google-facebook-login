f1d134aae88b182386b410effaf00e83a4ddb64c
Risan Ahamed <risanzahamed76@gmail.com>
2023-04-30 12:44:47 +0600

    first commit

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,16 +1,13 @@
-
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
-
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyB5Awy5pyW26-0tswiNw4GZe1eS9MYHeUg",
-  authDomain: "team-projects-4724e.firebaseapp.com",
-  projectId: "team-projects-4724e",
-  storageBucket: "team-projects-4724e.appspot.com",
-  messagingSenderId: "825594707242",
-  appId: "1:825594707242:web:255cc5b45c376b22ebf253"
+  apiKey: "AIzaSyAXDbxEO2yIdsYFge1IDaNXIQ5_ggXDhFc",
+  authDomain: "next-app-project.firebaseapp.com",
+  projectId: "next-app-project",
+  storageBucket: "next-app-project.appspot.com",
+  messagingSenderId: "561954031752",
+  appId: "1:561954031752:web:53e8ad9db9b89e03faf73d"
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
